e2c0c3f8e51fa9ead73539aff66ed44c9e32d38c
viraj <virajbutani1@gmail.com>
2024-11-19 22:52:30 +0530

    small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ import torusLight from "@/image/torusLight.png";
 import lightWightLight from "@/image/lightWightLight.png";
 import lightWightDark from "@/image/lightWightDark.png";
 import lightWightRlight from "@/image/lightWightRlight.png";
+import lightWightRdark from "@/image/lightWightRdark.png";
 import enduringDurabilityLight from "@/image/enduringDurabilityLight.png";
 import enduringDurabilityDark from "@/image/enduringDurabilityDark.png";
 import insulatingExcellenceLight from "@/image/insulatingExcellenceLight.png";
